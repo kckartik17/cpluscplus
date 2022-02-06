d604bdc2d95c930cfc2a7621b236a77c4475d096
Kartik Chauhan <kckartik17@gmail.com>
2022-02-06 17:23:47 +0530

    Day 12

---
+++ LeetCode/hard/60.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string getPermutation(int n, int k) {
+        int fact = 1;
+        vector<int> numbers;
+        for(int i = 1 ;i < n; i++){
+            fact = fact * i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(n);
+        string ans = "";
+        k = k - 1;
+        while(true){
+            ans = ans + to_string(numbers[k / fact]);
+            numbers.erase(numbers.begin() + k / fact);
+            if(numbers.empty()) break;
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+        return ans;
+}
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    cout << getPermutation(n,k);
+}
